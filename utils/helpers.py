f12e8971fdc342568ca637de97df357a4dea2653
IslomSobirov <atomm262@gmail.com>
2021-07-22 17:36:26 +0500

    added elastic functions to add/delete

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,11 @@
 import logging
 
 import settings
+import requests
+from envparse import env
+
+QuestionIndex = "question"
+ChoiceIndex = "choice"
 
 
 def get_logger(name: str='__main__', handler=None, formatter=None):
@@ -18,3 +23,17 @@ def get_logger(name: str='__main__', handler=None, formatter=None):
     logger.setLevel(settings.LOG_LEVEL)
 
     return logger
+
+async def addToElastic(index, id, data):
+    logger = get_logger(__name__)
+    url = f'{settings.ELASTIC_HOST}/{index}/_doc/{id}'
+    # url = url.format(settings.ELASTIC_HOST, index, id)
+    logger.debug(f'url for add is {url}')
+
+    requests.post(url, json=data)
+
+async def deleteFromElastic(index, id):
+    logger = get_logger(__name__)
+    url = f'{settings.ELASTIC_HOST}/{index}/_doc/{id}'
+    requests.delete(url)
+    logger.debug(f'url for add is {url}')
